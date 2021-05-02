eeb799b1c749c11350269efe3954a56439a782a8
Justin Ho <>
2021-05-02 20:13:16 +0000

    save 2

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,26 +1,34 @@
 package gr11review.part1;
 
 import java.io.*;
+import java.text.*;
 
 public class Review4 { 
     public static void main(String[] args) throws IOException {
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+      
+        DecimalFormat dollarFormat = new DecimalFormat("$###0.00");
+    int intitemamount;
+    double dblprice;
+    double dbltotal;
+    double dblsubtotal = 0;
+    double dbltax;
+  
+    System.out.print("How many items do you want to buy? ");
+    intitemamount = Integer.parseInt(key.readLine());
 
-    int intamountofitems;
-    int intitem1;
-    int intitem2;
-    int intitem3;
-    int intitem4;
+    for(int i = 0; i < intitemamount; i++) {
+      {
+      System.out.print("Enter the price of item number " + (i+1) + ": ");
+      dblprice = Double.parseDouble(key.readLine());
+          dblsubtotal = dblsubtotal + dblprice;
+         }
+       }
+        dbltax = dblsubtotal*0.13;
+        dbltotal = dblsubtotal + dbltax;
 
-    System.out.println("Enter the price for item 1: ");
-    intitem1 = Integer.parseInt(key.readLine());
-    System.out.println("Enter the price for item 2: ");
-    intitem2 = Integer.parseInt(key.readLine());
-    System.out.println("Enter the price for item 3: ");
-    intitem3 = Integer.parseInt(key.readLine());
-    System.out.println("Enter the price for item 4: ");
-
-    
-
+        System.out.println("Subtotal: " +dollarFormat.format(dblsubtotal));
+        System.out.println("Tax: " +dollarFormat.format(dbltax));
+        System.out.println("Total: " +dollarFormat.format(dbltotal));
     }
 }
--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,7 +1,31 @@
 package gr11review.part1;
 
 
+import java.io.*;
+
 public class Review5 { 
     public static void main(String[] args) throws IOException {
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+int intnumberofyears =0;
+double dblYIA; //YIA stands for yearly invested amount
+double dblCIR; //CIR stands for Compound Interest Rate 
+double dbltargetamount;
+double dblamountinvested = 0;
+double dbladdingstuff;
+
+System.out.print("Enter the yearly invested amount: ");
+dblYIA = Double.parseDouble(key.readLine());
+System.out.print("Enter the compound interest rate: ");
+dblCIR = Double.parseDouble(key.readLine());
+System.out.print("Enter the target amount: ");
+dbltargetamount = Double.parseDouble(key.readLine());
+
+while(dblamountinvested < dbltargetamount) {
+    intnumberofyears++;
+    dbladdingstuff = (dblCIR/dblYIA)*dblamountinvested;
+    dblamountinvested += dblYIA + dbladdingstuff;
+}
+System.out.println("The target amount will be earned in " +intnumberofyears+ " years.");
+    }
+}
